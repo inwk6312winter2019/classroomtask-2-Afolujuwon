a9444dc91c01a10c1948c453bb2ad72dcdbf840f
Afolujuwon <47113066+Afolujuwon@users.noreply.github.com>
2019-02-11 00:41:45 -0400

    update

--- subnetCalculator.py
+++ subnetCalculator.py
@@ -1,7 +1,19 @@
-def __init__(ip, network, mask,):
-	self.ip = input()
+Class ipCalc():
+def __init__(self,ip, network, mask,):
+	self.ipaddress = input()
 	self.network = input()
 	self.mask = input
-	
-	if '/' in self.ip:
-		self.ip
+
+    self.ipaddress=ipaddress
+
+ def belongsclass(ipaddress):
+    if int(ipaddress[0])>= 0 and int(ipaddress[0])<= 127:
+      print ("IP address belongs to class A")
+    elif int(ipaddress[0])>= 128 and int(ipaddress[0])<=191:
+      print ("IP address belongs to class B")
+    elif int(ipaddress[0])>=192 and int(ipaddress[0])<=223:
+      print("IP address belongs to class C")
+    elif int(ipaddress[0])>= 224 and int(ipaddress[0])<=239:
+      print("Ipaddress belongs to class D")
+n=str(input())
+melo=Network.belongsclass(n)
